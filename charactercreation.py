d920957e29ad5530309f45046d89aaa20eaa9653
Melinda Weed <melinda.weed@arm.com>
2018-06-16 20:57:03 +0300

    First .py  code

---
+++ charactercreation.py
@@ -0,0 +1,13 @@
+name = input("What is your name?")
+print(name)
+print("Create your character!")
+age = input("How old is your character?")
+strengths = input("How old is your character?")
+weaknesses = input("What are your character's weaknesses?")
+
+print("Your character's name is", name)
+print("Your character is", age, "years old")
+print("Strengths:", strengths)
+print("Weaknesses:", weaknesses)
+print(name, "says, 'Thanks for creating me!'")
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+No idea what will go here yet
